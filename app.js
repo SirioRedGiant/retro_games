fb4892a6fdd9826678de83cc2f89e0456b8b9d1b
MhZ9191 <hegic91@gmail.com>
2026-03-31 17:48:36 +0200

    Add fix router

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.use(cors({ origin: process.env.FRONTEND_URL }));
 const globalRouter = require("./routes/globalRouter");
 const products = require("./routes/productsRouter");
 const ordersRouter = require("./routes/ordersRouter");
-const order = require("./routes/orderRouter");
 // const register = require("./routes/registerRouter");
 // const login = require("./routes/loginRouter");
 // const user = require("./routes/userRouter");
@@ -28,7 +27,7 @@ app.use("/products", products);
 
 // ORDERS
 app.use("/shopping-cart", ordersRouter);
-app.use("/search", order);
+app.use("/search", ordersRouter);
 app.get("/", (req, res) => {
   res.json({
     success: true,
