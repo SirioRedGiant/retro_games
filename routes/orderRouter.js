5b9a53b0fae87c1340135d542fbbc65b15083b7e
MhZ9191 <hegic91@gmail.com>
2026-03-31 17:50:09 +0200

    DEL orderRouter

--- routes/orderRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const route = express.Router();
-const controller = require("../controllers/orderController");
-
-route.get("/order", controller.orderedBy);
-
-module.exports = route;
