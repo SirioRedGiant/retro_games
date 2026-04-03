b4a94bac29cf8b31cd0813ba23dd54ead592cbd2
MhZ9191 <hegic91@gmail.com>
2026-04-03 15:46:48 +0200

    Add fix

--- controllers/couponController.js
+++ controllers/couponController.js
@@ -2,7 +2,12 @@ const connect = require("../database/db");
 
 function sendCoupon(req, res) {
   const { code } = req.body;
-
+  if (typeof code != "string" || !code.trim()) {
+    return res.status(400).json({
+      success: false,
+      error: "Bad request",
+    });
+  }
   const sql =
     "select *,DATE(start_date) as start ,DATE(end_date) as end from coupons where code=?";
 
@@ -13,18 +18,27 @@ function sendCoupon(req, res) {
         error: "Internal server error",
       });
 
-    const today = new Date().toISOString().slice(0, 10);
-    const start = new Date(resultQuery[0].start).toISOString().slice(0, 10);
-    const end = new Date(resultQuery[0].end).toISOString().slice(0, 10);
+    if (resultQuery.length > 0) {
+      const today = new Date().toISOString().slice(0, 10);
+      const start = new Date(resultQuery[0]?.start).toISOString().slice(0, 10);
+      const end = new Date(resultQuery[0]?.end).toISOString().slice(0, 10);
 
-    if (today > start && today < end) {
-      return res.json({
-        success: true,
-        result: {
-          valid: true,
-          discount: resultQuery[0].discount,
-        },
-      });
+      if (today > start && today < end) {
+        return res.json({
+          success: true,
+          result: {
+            valid: true,
+            discount: resultQuery[0].discount,
+          },
+        });
+      } else {
+        return res.json({
+          success: false,
+          result: {
+            valid: false,
+          },
+        });
+      }
     } else {
       return res.json({
         success: false,
